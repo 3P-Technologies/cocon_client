13f398cd6700a9a9c942756db7dcc58de78e0ba5
MarcoMiano <58668074+MarcoMiano@users.noreply.github.com>
2025-08-18 16:46:59 +0200

    [TEST] introduce stupid smoke test usefull for github actions
    Signed-off-by: MarcoMiano <58668074+MarcoMiano@users.noreply.github.com>

---
+++ test_import.py
@@ -0,0 +1,13 @@
+# TELEVIC CoCon CLIENT
+# test_import.py
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 3P Technologies Srl
+# Author: Marco Miano
+import cocon_client
+from importlib import metadata
+
+print("import ok")
+try:
+    print("dist version:", metadata.version("cocon_client"))
+except metadata.PackageNotFoundError:
+    print("version metadata not found (not installed?)")
